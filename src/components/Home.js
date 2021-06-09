1419252bcfee4100de7962dcd92b398eb967105f
Brianna Gordon <brigordon574@gmail.com>
2021-06-09 13:30:54 -0700

    commit tree 3688449c6647070e4d85f0a4638d5e8faccf6fd3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+
+const theStyle ={
+  color:'firebrick',
+}
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div id ="home">
+    <h1 style={theStyle}> {name} is a Web Developer from  {city} </h1>
+    </div>
+  );
 }
 
 export default Home;
